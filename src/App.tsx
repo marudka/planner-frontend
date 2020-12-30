829b570efff34be95d9607f439ce7ff17d14dbb7
anna.welc <it123@g2a.com>
2020-12-30 23:35:09 +0100

    debugging

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import {
   Route,
   Link
 } from 'react-router-dom';
-import { LaptopOutlined, FileAddOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+// import { LaptopOutlined, FileAddOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { Menu, Layout } from 'antd';
 import './App.css';
 
 import { Home } from './Home';
-import { AddRecipeConnector } from './AddRecipeConnector';
-import { ShoppingList } from './ShoppingList';
+// import { AddRecipeConnector } from './AddRecipeConnector';
+// import { ShoppingList } from './ShoppingList';
 
 const { Header } = Layout;
 
@@ -24,19 +24,19 @@ function App() {
             <img alt='logo' src={'http://bluewolf.pl/wp-content/themes/bluewolf/fonts/logo.svg'} width='50px' height='50px' />
           </div>
           <Menu theme={'dark'} mode={'horizontal'}>
-            <Menu.Item key={1}><Link to={'/'}><LaptopOutlined />Home</Link></Menu.Item>
-            <Menu.Item key={2}><Link to={'/add-recipe'}><FileAddOutlined />Add recipe</Link></Menu.Item>
-            <Menu.Item key={3}><Link to={'/shopping-list'}><ShoppingCartOutlined />Daily shopping list</Link></Menu.Item>
+            {/*<Menu.Item key={1}><Link to={'/'}><LaptopOutlined />Home</Link></Menu.Item>*/}
+            {/*<Menu.Item key={2}><Link to={'/add-recipe'}><FileAddOutlined />Add recipe</Link></Menu.Item>*/}
+            {/*<Menu.Item key={3}><Link to={'/shopping-list'}><ShoppingCartOutlined />Daily shopping list</Link></Menu.Item>*/}
           </Menu>
         </Header>
       </Layout>
       <Switch>
-        <Route path={'/add-recipe'}>
-          <AddRecipeConnector />
-        </Route>
-        <Route path={'/shopping-list'}>
-          <ShoppingList />
-        </Route>
+        {/*<Route path={'/add-recipe'}>*/}
+        {/*  <AddRecipeConnector />*/}
+        {/*</Route>*/}
+        {/*<Route path={'/shopping-list'}>*/}
+        {/*  <ShoppingList />*/}
+        {/*</Route>*/}
         <Route path={'/'}>
           <Home />
         </Route>
