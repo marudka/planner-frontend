805d47d4c9b19fd0552e87237c2199d4fef02d3a
anna.welc <it123@g2a.com>
2020-12-31 13:55:24 +0100

    debugging

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,4 @@
-const BASE_URL = `http://${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_PORT ? ':' + process.env.REACT_APP_PORT : ''}`;
+const BASE_URL = `${process.env.REACT_APP_PROTOCOL ? process.env.REACT_APP_PROTOCOL : ''}${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_PORT ? ':' + process.env.REACT_APP_PORT : ''}`;
 
 export {
   BASE_URL
