7247edae7e2d0f4693c652348821674b74778d91
anna.welc <it123@g2a.com>
2021-01-02 20:00:23 +0100

    improvements

--- src/DrawerContent.tsx
+++ src/DrawerContent.tsx
@@ -1,26 +1,17 @@
-import React, { FunctionComponent, useState } from 'react';
-import { Typography, Descriptions, Image, Input, Form, Select, Button, Tag, Empty } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import React, { FunctionComponent } from 'react';
+import { Typography, Descriptions, Image, Tag, Empty } from 'antd';
 import { useRecipesContext } from './useRecipesContext';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import {fetchData, MethodType} from './useFetch';
-import {BASE_URL} from "./constants/config";
+import { EditForm } from './EditForm';
 
 const { Title } = Typography;
-const { Option} = Select;
 
 interface DrawerContentProps {
   id: string;
   isEditMode: boolean;
 }
 
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
-};
-
 export const DrawerContent: FunctionComponent<DrawerContentProps> = ({ id, isEditMode }) => {
-  const { recipes, setRecipes } = useRecipesContext();
+  const { recipes } = useRecipesContext();
   const chosenRecipe = recipes.find((item) => item._id === id);
 
   if (!chosenRecipe) {
@@ -29,102 +20,22 @@ export const DrawerContent: FunctionComponent<DrawerContentProps> = ({ id, isEdi
 
   const list = chosenRecipe.ingredients.map((item, key) => <li key={key}>{item.name} {item.count}, {item.unit}</li>);
 
-  const onFinish = () => {
-    console.log('test');
-  };
-
-  const onFinishFailed = () => {
-    console.log('test finish');
-  };
-
-  const handleChange = () => {
-
-  };
-
-  const handleBlur = () => {
-
-  };
-
-  const handleIngredientNameChange = () => {
-
-  };
-
-  const handleIngredientCountChange = () => {
-
-  };
-
-  const handleDeleteRecipe = () => {
-    fetchData(`${BASE_URL}/recipes/${id}`, MethodType.DELETE).then(() => {
-      const index = recipes.findIndex((item) => item._id === id);
-      const copyRecipes = [...recipes];
-      copyRecipes.splice(index, 1);
-      setRecipes(copyRecipes);
-    });
-  };
-
   if (isEditMode) {
-    return (
-      <div>
-        <Form
-          {...layout}
-          name='basic'
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-        >
-          <Form.Item
-            label='Name'
-            rules={[{ required: true, message: 'Please input recipe name!' }]}
-          >
-            <Input value={chosenRecipe.name} defaultValue={chosenRecipe.name} name='name' onChange={handleChange} onBlur={handleBlur} />
-          </Form.Item>
-
-          <Form.Item
-            label='Description'
-            rules={[{ required: false }]}
-          >
-            <Input.TextArea value={chosenRecipe.description} defaultValue={chosenRecipe.name} name='description' onChange={handleChange} onBlur={handleBlur} />
-          </Form.Item>
-
-          {list}
-
-          <Form.Item label='Add ingredient'>
-            <Input.Group compact>
-              <Input style={{ width: '200px' }} />
-              <Input style={{ width: '80px' }} />
-              <Select defaultValue="gram">
-                <Option value="gram">gram</Option>
-                <Option value="unit">unit</Option>
-              </Select>
-              <Button type="primary"><PlusOutlined /></Button>
-            </Input.Group>
-          </Form.Item>
-
-          <div>
-            <Button htmlType='submit' icon={<EditOutlined />} style={{ marginRight: '8px' }}>
-              Edit recipe
-            </Button>
-            <Button danger icon={<DeleteOutlined />} onClick={handleDeleteRecipe}>
-              Remove recipe
-            </Button>
-          </div>
-        </Form>
-      </div>
-    );
+    return <EditForm name={chosenRecipe.name} description={chosenRecipe.description} id={id} />;
   }
 
   return (
-    <div>
+    <>
       {chosenRecipe.image && (
-        <Image width='100%' height='120' src={chosenRecipe.image} />
+        <Image width='100%' height='120' src={chosenRecipe.image} style={{ marginBottom: '12px' }} />
       )}
       <Tag color='blue'>Vegetarian</Tag>
-      <Title level={3}>{chosenRecipe.name}</Title>
+      <Title level={3} style={{ marginTop: '10px' }}>{chosenRecipe.name}</Title>
       {chosenRecipe.description && <Descriptions.Item label='Description'>{chosenRecipe.description}</Descriptions.Item>}
       <Title level={5}>Ingredients</Title>
       <ul>
         {list}
       </ul>
-    </div>
+    </>
   )
 };
---
+++ src/EditForm.tsx
@@ -0,0 +1,169 @@
+import React, { FunctionComponent, useState } from 'react';
+import { Input, Form, Select, Button, Tag, Empty } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { useRecipesContext } from './useRecipesContext';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { fetchData, MethodType } from './useFetch';
+import { RecipeType, Ingredient } from './Recipes';
+import { BASE_URL } from './constants/config';
+import { FormikErrors, FormikProps, withFormik } from 'formik';
+
+const { Option } = Select;
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+
+interface FormValues {
+  name: string;
+  description: string;
+  ingredients: Ingredient[];
+  id: string;
+}
+
+interface DrawerContentProps {
+  id: string;
+  chosenRecipe: RecipeType;
+}
+
+interface Props {
+  // submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
+  name: string;
+  description?: string;
+  ingredients?: Ingredient[];
+  id: string;
+}
+
+const Edit: FunctionComponent<FormikProps<FormValues> & Props> = ({ values, handleChange }) => {
+  const { recipes, setRecipes } = useRecipesContext();
+  const [ingredients, setIngredients] = useState(values.ingredients);
+
+  const list = values.ingredients.map((item, key) =>
+    <li key={key}>{item.name} {item.count}, {item.unit} <b>minus</b></li>);
+
+  const onFinish = () => {
+    const body = {
+      name: values.name,
+      description: values.description,
+      ingredients: ingredients
+    };
+
+    fetchData(`${BASE_URL}/recipes/${values.id}`, MethodType.PATCH, body).then(() => {
+      const changedRecipes = recipes.map((item) => {
+        if (item._id === values.id) {
+          return {
+            ...item,
+            name: values.name,
+            description: values.description,
+            ingredients: ingredients
+          }
+        }
+        return item;
+      });
+      setRecipes(changedRecipes);
+    });
+  };
+
+  const onFinishFailed = () => {
+    console.log('test finish');
+  };
+
+  const handleBlur = () => {
+
+  };
+
+  const handleIngredientNameChange = () => {
+
+  };
+
+  const handleIngredientCountChange = () => {
+
+  };
+
+  const handleDeleteRecipe = () => {
+    fetchData(`${BASE_URL}/recipes/${values.id}`, MethodType.DELETE).then(() => {
+      const index = recipes.findIndex((item) => item._id === values.id);
+      const copyRecipes = [...recipes];
+      copyRecipes.splice(index, 1);
+      setRecipes(copyRecipes);
+    });
+  };
+
+  return (
+    <div>
+      <Form
+        {...layout}
+        name='basic'
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label='Name'
+          rules={[{ required: true, message: 'Please input recipe name!' }]}
+        >
+          <Input value={values.name} defaultValue={values.name} name='name' onChange={handleChange} onBlur={handleBlur} />
+        </Form.Item>
+
+        <Form.Item
+          label='Description'
+          rules={[{ required: false }]}
+        >
+          <Input.TextArea value={values.description} defaultValue={values.name} name='description' onChange={handleChange} onBlur={handleBlur} />
+        </Form.Item>
+
+        {list}
+
+        <Form.Item label='Add ingredient'>
+          <Input.Group compact>
+            <Input />
+            <Input />
+            <Select defaultValue='gram'>
+              <Option value='gram'>gram</Option>
+              <Option value='unit'>unit</Option>
+            </Select>
+            <Button type='primary'><PlusOutlined /></Button>
+          </Input.Group>
+        </Form.Item>
+
+        <div>
+          <Button htmlType='submit' icon={<EditOutlined />} style={{ marginRight: '8px' }}>
+            Edit recipe
+          </Button>
+          <Button danger icon={<DeleteOutlined />} onClick={handleDeleteRecipe}>
+            Remove recipe
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export const EditForm = withFormik<Props, FormValues>({
+  mapPropsToValues: (props) => ({
+    name: props.name,
+    description: props.description || '',
+    ingredients: props.ingredients || [],
+    id: props.id
+  }),
+
+  // Custom sync validation
+  validate: values => {
+    const errors = {};
+
+    if (!values.name) {
+      // @ts-ignore
+      errors.name = 'Required';
+    }
+
+    return errors;
+  },
+
+  handleSubmit: (values, { setSubmitting }) => {
+    setSubmitting(false);
+  },
+
+  displayName: 'EditForm',
+  enableReinitialize: true,
+})(Edit);
